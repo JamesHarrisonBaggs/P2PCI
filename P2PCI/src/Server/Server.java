b05fab56606a5eef8943b318f1597063bc29d4f9
Mingkang Zhuang <Mingkang Zhuang@DESKTOP-D6SQPLO>
2017-06-09 16:55:13 -0400

    add messageParser

--- P2PCI/src/Server/Server.java
+++ P2PCI/src/Server/Server.java
@@ -1,5 +1,15 @@
 package Server;
 
-public class Server {
+import java.util.*;
 
+public class Server {
+	
+	private LinkedList<Peer> Peers;
+	
+	private LinkedList<RFC> RFCs;
+	
+	public Server() {
+		
+	}
+	
 }
