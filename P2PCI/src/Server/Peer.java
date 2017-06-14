f4271f0ffc69a3484b87a611591267bceaee2097
JHBaggs <jhbaggs@ncsu.edu>
2017-06-14 13:44:50 -0400

    Peer Changes (and testing examples)

--- P2PCI/src/Server/Peer.java
+++ P2PCI/src/Server/Peer.java
@@ -3,10 +3,68 @@ package Server;
 import java.io.*;
 import java.net.*;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Peer {
 
 	public static void main(String[] args) throws InterruptedException {
+		File workingDir = new File("./");
+		File[] list1 = workingDir.listFiles();
+		String[] list2 = workingDir.list();
+		
+		for(String s: list2 ){
+			System.out.println(s);
+		}
+		
+		for(File f: list1){
+			System.out.println(f.getName());
+		}
+	}
+		
+	
+	public static ServerSocket getServerSocket() throws InterruptedException{
+		ServerSocket uSocket = null;
+		try {
+			uSocket = new ServerSocket(7735);
+		} catch (IOException e) {
+			System.out.println("Exception thrown");
+			e.printStackTrace();
+		}
+		Thread.sleep(10000);
+		
+		return uSocket;
+	}
+
+	public static void directoryTestExample() {
+		// Directory Creation and Deletion Example
+
+		File path = new File("./peer31");
+		boolean success = (path.mkdirs());
+		if (success) {
+			System.out.println("Directory created");
+			System.out.println("Press \"ENTER\" to continue...");
+			Scanner scanner = new Scanner(System.in);
+			scanner.nextLine();
+			scanner.close();
+
+			try {
+				success = deleteRecursive(path);
+			} catch (FileNotFoundException e) {
+				System.out.println("Problem deletingRecursively: " + e);
+			}
+
+			if (success) {
+				System.out.println("Success Deleting Directory");
+			} else {
+				System.out.println("Directory not delted.");
+			}
+
+		} else {
+			System.out.println("Directory not created");
+		}
+	}
+
+	public static void socketsTestExample() throws InterruptedException {
 		// Modified example socket program from
 		// http://www.javaworld.com/article/2077322/core-java/core-java-sockets-programming-in-java-a-tutorial.html
 
@@ -48,5 +106,21 @@ public class Peer {
 			}
 
 		}
+
+	}
+
+	// Taken verbatim (with a minor change) from Paulitex's recursive directory
+	// deletion solution on
+	// https://stackoverflow.com/questions/779519/delete-directories-recursively-in-java
+	public static boolean deleteRecursive(File path) throws FileNotFoundException {
+		if (!path.exists())
+			throw new FileNotFoundException(path.getAbsolutePath());
+		boolean ret = true;
+		if (path.isDirectory()) {
+			for (File f : path.listFiles()) {
+				ret = ret && deleteRecursive(f);
+			}
+		}
+		return ret && path.delete();
 	}
 }
