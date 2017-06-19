dadb7fec778cdbeef42cc956ed38f7bfea85221f
JHBaggs <jhbaggs@ncsu.edu>
2017-06-18 22:35:58 -0400

    p2sthread

--- P2PCI/src/Server/PeerToServerThread.java
+++ P2PCI/src/Server/PeerToServerThread.java
@@ -1,9 +1,16 @@
 package Server;
 
+import java.io.IOException;
+
 public class PeerToServerThread implements Runnable{
 	
 	public void run(){
-		
+		try {
+			System.out.println("p2sthread");
+			Peer.handleConsoleCommands();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
